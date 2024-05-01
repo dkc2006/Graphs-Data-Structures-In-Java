ad5491dfa3a4725cfdcc0355bc9c8c14e61b8f47
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-05-01 06:58:55 +0530

    Has Path Or Not?

---
+++ src/main/java/problemsOnGraphs/HasPath.java
@@ -0,0 +1,73 @@
+package problemsOnGraphs;
+
+import java.util.ArrayList;
+
+public class HasPath {
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int index = 0; index < graph.length; index++) {
+            graph[index] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    public static boolean hasPath(ArrayList<Edge>[] graph, int source, int destination, boolean[] visited) {
+        if (source == destination) {
+            return true;
+        }
+        visited[source] = true;
+        for (Edge edge : graph[source]) {
+            if (!visited[edge.destination] && hasPath(graph, edge.destination, destination, visited)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+         /*
+        1----3
+      /      | \
+      0      |  5 ---6
+      \      | /
+       2-----4
+        */
+
+        int vertices = 7; // Number of vertices in the graph
+        ArrayList<Edge>[] graph = new ArrayList[vertices];
+        createGraph(graph);
+        System.out.println(hasPath(graph, 0, 5, new boolean[vertices]));
+    }
+
+    static class Edge {
+        int source;
+        int destination;
+        int weight;
+
+        public Edge(int source, int destination, int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+}
